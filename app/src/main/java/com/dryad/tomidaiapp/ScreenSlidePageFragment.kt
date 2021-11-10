bb716e85fb28b0f27c40ee49b417aaea1898e3c8
Jun11970256 <s1970256@gmail.com>
2021-11-10 14:02:04 +0900

    カルーセル作成

---
+++ app/src/main/java/com/dryad/tomidaiapp/ScreenSlidePageFragment.kt
@@ -0,0 +1,22 @@
+package com.dryad.tomidaiapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivities
+import androidx.fragment.app.Fragment
+
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_screen_slide_page, container, false)
+    }
+}
+
---
+++ app/src/main/java/com/dryad/tomidaiapp/ScreenSlidePageFragment2.kt
@@ -0,0 +1,18 @@
+package com.dryad.tomidaiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ScreenSlidePageFragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_screen_slide_page2, container, false)
+
+}
