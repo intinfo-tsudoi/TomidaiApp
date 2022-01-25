8e9b1250f82d116c93c4a83f9164cc3780efc556
morio-23 <morio_sax@yahoo.co.jp>
2022-01-25 22:33:13 +0900

    WebView,PDFViewをまとめました

--- app/src/main/java/com/dryad/tomidaiapp/WebViewActivity.kt
+++ app/src/main/java/com/dryad/tomidaiapp/WebViewActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 
-class BusActivity : AppCompatActivity() {
+class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bus)
+        setContentView(R.layout.activity_web_view)
+
+        val intent = intent
+        val getURL = intent.getStringExtra("URL").toString()
+        println(getURL)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val webView: WebView = findViewById(R.id.webView)
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://www.u-toyama.ac.jp/studentsupport/student-support/shuttle_bus/")
+        webView.loadUrl(getURL)
     }
 }
